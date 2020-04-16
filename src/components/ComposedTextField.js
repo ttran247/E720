6e1cab1b297568eac823abbc4b5b7b78112d5811
Tommy Tran <ttran247@gmail.com>
2020-04-15 22:23:52 -0400

    update page

--- src/components/ComposedTextField.js
+++ src/components/ComposedTextField.js
@@ -4,9 +4,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 
-
  function ComposedTextField() {
-    const [name, setName] = React.useState('Composed TextField');
+    const [name, setName] = React.useState('');
 
     const handleChange = (event) => {
         setName(event.target.value);
@@ -14,14 +13,37 @@ import OutlinedInput from '@material-ui/core/OutlinedInput';
 
      return (
         <>
-             <h1>Contact</h1>
-             <h2>Reach out to us about business opportunities, media inquiries, or just to talk. Looking for job opportunities? Head over to Careers.</h2>
+             <h1 className="contact">Contact</h1>
+             <h2 className="contact">Reach out to us about business opportunities, media inquiries, or just to talk. Looking for job opportunities? Head over to Careers.</h2>
         <form className="form" noValidate autoComplete="off">
 
             <FormControl variant="outlined">
-                <InputLabel htmlFor="component-outlined">Name</InputLabel>
+                <InputLabel htmlFor="component-outlined">First Name</InputLabel>
                 <OutlinedInput id="component-outlined" value={name} onChange={handleChange} label="Name" />
-            </FormControl>
+                 </FormControl>
+                 <br/>
+                 <FormControl variant="outlined">
+                     <InputLabel htmlFor="component-outlined">Last Name</InputLabel>
+                     <OutlinedInput id="component-outlined" value={name} onChange={handleChange} label="Name" />
+                 </FormControl>
+                 <br />
+
+                 <FormControl variant="outlined">
+                     <InputLabel htmlFor="component-outlined">Company</InputLabel>
+                     <OutlinedInput id="component-outlined" value={name} onChange={handleChange} label="Name" />
+                 </FormControl>
+                 <br />
+
+                 <FormControl variant="outlined">
+                     <InputLabel htmlFor="component-outlined">Email</InputLabel>
+                     <OutlinedInput id="component-outlined" value={name} onChange={handleChange} label="Name" />
+                 </FormControl>
+                 <br />
+
+                 <FormControl variant="outlined">
+                     <InputLabel htmlFor="component-outlined">Phone</InputLabel>
+                     <OutlinedInput id="component-outlined" value={name} onChange={handleChange} label="Name" />
+                 </FormControl>
 
              </form>
              </>
