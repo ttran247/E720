aa792534cf57552753f0a80da1f186d018740f5b
Tommy Tran <ttran247@gmail.com>
2020-04-16 20:02:29 -0400

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ function footer () {
             <div className="footerContainer">
                 <div className="text-container2">
                 <h4 className="footertext">About Us</h4>
-                <h4 className="footertext">Career </h4>
-                 <h4 className="footertext">Partners </h4>
-                <h4 className="footertext">Phone: (111)500-500 </h4>
                 <h4 className="footertext">Email: contact@entertainment720.com</h4>
+                <h4 className="footertext">Career </h4>
+                <h4 className="footertext">Phone: (111)500-500 </h4>
+                 <h4 className="footertext">Partners </h4>
                     <h4 className="footertext">Address: 602 Trumpball Street Pawnee, IN</h4> 
             
                 </div>
